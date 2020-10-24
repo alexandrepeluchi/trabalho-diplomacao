744029d02ba0f87291156ccc8a7b31f86dd23bcc
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-10-24 16:16:09 -0300

    DataAnnotation para obrigatoriedade em algumas propriedades

--- TrabalhoDiplomacao/UsuarioService/Database/Entities/Usuario.cs
+++ TrabalhoDiplomacao/UsuarioService/Database/Entities/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,16 +9,23 @@ namespace UsuarioService.Database.Entities
     public class Usuario
     {
         public int UsuarioId { get; set; }
+        [Required]
         public string Nome { get; set; }
+        [Required]
         public string Sobrenome { get; set; }
+        [Required]
         public string CPF { get; set; }
         public string RG { get; set; }
         public string Email { get; set; }
+        [Required]
         public string Telefone { get; set; }
         public bool Proprietario { get; set; }
         public bool Status { get; set; }
+        [Required]
         public string Username { get; set; }
-        public string Password { get; set; }     
+        [Required]
+        public string Password { get; set; }
+        [Required]
         public DateTime DataRegistro { get; set; }
     }
 }
