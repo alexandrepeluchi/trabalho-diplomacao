dfbd280c69d0a6d554796b03b5e1aed317fcc08b
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-10-24 15:57:24 -0300

    Criado o DBContext para gerar a entidade Usuários no Banco de Dados

---
+++ TrabalhoDiplomacao/UsuarioService/Database/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsuarioService.Database.Entities;
+
+namespace UsuarioService.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=GW\\SQLEXPRESS; Database=TD; User ID=sa; Password=123qwe;");
+        }
+    }
+}
