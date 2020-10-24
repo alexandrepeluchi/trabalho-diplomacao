00126e0fba400e3a6051cd0bceb8ca7c6cd16349
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-10-24 15:45:17 -0300

    Entidade Usuario

---
+++ TrabalhoDiplomacao/UsuarioService/Database/Entities/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsuarioService.Database.Entities
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string CPF { get; set; }
+        public string RG { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public bool Status { get; set; }
+        public DateTime DataRegistro { get; set; }
+        public bool Proprietario { get; set; }
+    }
+}
