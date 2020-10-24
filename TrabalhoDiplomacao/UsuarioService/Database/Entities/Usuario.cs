472231ae4d6b4a16925cee4a1fdf61fbf2f30dc1
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-10-24 15:57:00 -0300

    Algumas alterações na entidade Usuário

--- TrabalhoDiplomacao/UsuarioService/Database/Entities/Usuario.cs
+++ TrabalhoDiplomacao/UsuarioService/Database/Entities/Usuario.cs
@@ -12,11 +12,12 @@ namespace UsuarioService.Database.Entities
         public string Sobrenome { get; set; }
         public string CPF { get; set; }
         public string RG { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
         public string Email { get; set; }
+        public string Telefone { get; set; }
+        public bool Proprietario { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }     
         public bool Status { get; set; }
         public DateTime DataRegistro { get; set; }
-        public bool Proprietario { get; set; }
     }
 }
