6e3f6ab614da5e5a637255c111a4d37d43b02c0e
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-11-02 11:26:53 -0300

    DbContext do microserviço Pedido

---
+++ TrabalhoDiplomacao/PedidoService/Database/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PedidoService.Database.Entities
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Pedido> Pedidos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=GW\\SQLEXPRESS; Database=TD; User ID=sa; Password=123qwe;");
+        }
+    }
+}
