d53d91da65f99aa8fd0b955b1a28eed7adb21302
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-11-02 11:26:39 -0300

    Adicionado entidade Pedido

---
+++ TrabalhoDiplomacao/PedidoService/Database/Entities/Pedido.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using UsuarioService.Database.Entities;
+
+namespace PedidoService.Database.Entities
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public float Valor { get; set; }
+        public bool Status { get; set; }
+        public DateTime DataPedido { get; set; }
+        public long UsuarioId { get; set; }
+        [ForeignKey("UsuarioId")]
+        public Usuario Usuario { get; set; }
+    }
+}
