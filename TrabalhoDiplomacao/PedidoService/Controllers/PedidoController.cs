89fcb099dbd9544ec51c601cc5101197ca113d9e
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-11-02 21:18:58 -0300

    Controlador para a entidade Pedidos

--- TrabalhoDiplomacao/PedidoService/Controllers/PedidoController.cs
+++ TrabalhoDiplomacao/PedidoService/Controllers/PedidoController.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PedidoService.Database.Entities;
+using PedidoService.Services;
+using UsuarioService.Database.Entities;
 
 namespace PedidoService.Controllers
 {
@@ -13,25 +16,34 @@ namespace PedidoService.Controllers
     public class PedidoController : ControllerBase
     {
         ApplicationDbContext db;
-        public PedidoController()
+        private IPedidoManager _pedidoManager;
+
+        public PedidoController(IPedidoManager pedidoManager)
         {
             db = new ApplicationDbContext();
+            _pedidoManager = pedidoManager;
         }
 
+        [Authorize(Roles = Roles.Admin)]
         [HttpGet]
         public IEnumerable<Pedido> GetAll()
         {
             return db.Pedidos.ToList();
         }
 
-        // GET api/<PedidoController>/5
+        [Authorize(Roles = Roles.Admin)]
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(long id)
         {
-            return "value";
+            var pedido = db.Pedidos.Find(id);
+
+            if (pedido == null)
+                return BadRequest(new { message = "Pedido não foi encontrado!" });
+
+            return Ok(pedido);
         }
 
-        // POST api/<PedidoController>
+        [Authorize(Roles = Roles.Admin)]
         [HttpPost]
         public IActionResult Post([FromBody] Pedido model)
         {
