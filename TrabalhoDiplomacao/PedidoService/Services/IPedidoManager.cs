4888a0ff909da99f4ded4c9801b42177c2d14f28
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-11-02 21:18:46 -0300

    Criação do manager de Pedidos

---
+++ TrabalhoDiplomacao/PedidoService/Services/IPedidoManager.cs
@@ -0,0 +1,14 @@
+﻿using PedidoService.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PedidoService.Services
+{
+    public interface IPedidoManager
+    {
+        Pedido BuscaPorId(int id);
+        IEnumerable<Pedido> BuscaTodos();
+    }
+}
