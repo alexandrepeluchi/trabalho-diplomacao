cc9bd1c80d93b0a967b48c7fa086963a9a4476f3
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-11-02 22:32:29 -0300

    Validador e manager de Pedidos

---
+++ TrabalhoDiplomacao/PedidoService/Validacao/PedidoValidador.cs
@@ -0,0 +1,35 @@
+﻿using PedidoService.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsuarioService.Database.Entities;
+using UsuarioService.Services;
+
+namespace PedidoService.Validacao
+{
+    public class PedidoValidador : IPedidoValidador
+    {
+        private IUsuarioManager _usuarioManager;
+
+        public PedidoValidador(IUsuarioManager usuarioManager)
+        {
+            _usuarioManager = usuarioManager;
+        }
+
+        public void ValidaRegrasDeNegocio(Pedido pedido)
+        {
+            VerificaDocumentos(pedido);
+        }
+
+        private void VerificaDocumentos(Pedido pedido)
+        {
+            var usuarioNoDb = _usuarioManager.BuscaPorId(pedido.UsuarioId);
+
+            if (usuarioNoDb == null)
+            {
+                throw new Exception("O Usuário não foi encontrado.");
+            }
+        }
+    }
+}
