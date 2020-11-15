41ab6965338fa58738d2d414e1fd8d4b470dad8c
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-11-15 15:28:17 -0300

    DTO's para busca e criação de um produto

--- TrabalhoDiplomacao/PedidoService/Models/Produtos/CriaProdutoBindingModel.cs
+++ TrabalhoDiplomacao/PedidoService/Models/Produtos/CriaProdutoBindingModel.cs
@@ -1,15 +1,18 @@
-﻿using System;
+﻿using PedidoService.Database.Entities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace PedidoService.Models
+namespace PedidoService.Models.Produtos
 {
-    public class ProdutoDTO
+    public class CriaProdutoBindingModel
     {
-        public int Id { get; set; }
+        [Required]
         public string Nome { get; set; }
         public string Descricao { get; set; }
+        [Required]
         public float Preco { get; set; }
         public string Codigo { get; set; }
     }
