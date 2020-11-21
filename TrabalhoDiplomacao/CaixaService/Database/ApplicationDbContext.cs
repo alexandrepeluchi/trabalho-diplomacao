9685c267952daf3073f41347d014783883f44917
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-11-21 17:49:03 -0300

    Criação da entidade Caixa e migration inicial

---
+++ TrabalhoDiplomacao/CaixaService/Database/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CaixaService.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaixaService.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Caixa> Caixas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=GW\\SQLEXPRESS; Database=TD; User ID=sa; Password=123qwe;");
+        }
+    }
+}
---
+++ TrabalhoDiplomacao/CaixaService/Database/Entities/Caixa.cs
@@ -0,0 +1,22 @@
+﻿using PedidoService.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaixaService.Database.Entities
+{
+    [Table("Caixas")]
+    public class Caixa
+    {
+        public int Id { get; set; }
+        public float SaldoInicial { get; set; }
+        public float? SaldoFinal { get; set; }
+        public DateTime HoraAbertura { get; set; }
+        public DateTime? HoraEncerramento { get; set; }
+        public string Descricao { get; set; }
+        public string Observacao { get; set; }
+        public bool Status { get; set; }
+    }
+}
