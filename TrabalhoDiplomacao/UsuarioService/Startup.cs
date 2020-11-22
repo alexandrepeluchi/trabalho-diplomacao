4e05692d5cb34a06f7e445a89c9301e871ac2c1f
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-11-22 16:04:30 -0300

    Configuração do automapper no Startup do serviço usuario

--- TrabalhoDiplomacao/UsuarioService/Startup.cs
+++ TrabalhoDiplomacao/UsuarioService/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -57,6 +58,13 @@ namespace UsuarioService
                     ValidateAudience = false
                 };
             });
+
+            services.AddControllersWithViews()
+                .AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
+
+            services.AddAutoMapper(typeof(Startup));
 
             // configure DI for application services
             services.AddScoped<IUsuarioManager, UsuarioManager>();
