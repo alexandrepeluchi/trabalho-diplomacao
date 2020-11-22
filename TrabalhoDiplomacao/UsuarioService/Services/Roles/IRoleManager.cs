71af4f6b2f3121eef2b48cb82a3acc7e040ba2ba
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-11-22 16:43:25 -0300

    Manager da entidade Roles

---
+++ TrabalhoDiplomacao/UsuarioService/Services/Roles/IRoleManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsuarioService.Database.Entities;
+
+namespace UsuarioService.Services.Roles
+{
+    public interface IRoleManager
+    {
+        Role BuscaPorId(int id);
+        IEnumerable<Role> BuscaTodos();
+    }
+}
