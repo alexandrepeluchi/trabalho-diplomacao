3a35feef2ca4d08558a7dcf38dbe913df8abd59d
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-11-22 15:43:35 -0300

    DTOs e Mapeamento

---
+++ TrabalhoDiplomacao/CaixaService/Mapeamentos/CaixaProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using CaixaService.Database.Entities;
+using CaixaService.Models.Caixas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaixaService.Mapeamentos
+{
+    public class CaixaProfile : Profile
+    {
+        public CaixaProfile()
+        {
+            CreateMap<Caixa, CaixaOutputDTO>();
+
+            CreateMap<CriaCaixaBindingModel, Caixa>();
+
+            CreateMap<CaixaBindingModel, Caixa>().ReverseMap();
+        }
+    }
+}
