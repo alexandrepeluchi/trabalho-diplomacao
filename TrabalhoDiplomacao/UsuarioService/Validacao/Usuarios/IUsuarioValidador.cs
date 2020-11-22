01ebe0d4a9b82a7282e89fb10e352282fa32df97
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-11-22 16:41:02 -0300

    Validação de documento do usuário

---
+++ TrabalhoDiplomacao/UsuarioService/Validacao/Usuarios/IUsuarioValidador.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsuarioService.Database.Entities;
+
+namespace UsuarioService.Validacao.Usuarios
+{
+    public interface IUsuarioValidador
+    {
+        void ValidaRegrasDeNegocio(Usuario usuario);
+    }
+}
---
+++ TrabalhoDiplomacao/UsuarioService/Validacao/Usuarios/UsuarioValidador.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsuarioService.Database.Entities;
+using UsuarioService.Services.Roles;
+
+namespace UsuarioService.Validacao.Usuarios
+{
+    public class UsuarioValidador : IUsuarioValidador
+    {
+        private readonly IRoleManager _roleManager;
+
+        public UsuarioValidador(IRoleManager roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public void ValidaRegrasDeNegocio(Usuario usuario)
+        {
+            VerificaDocumentos(usuario);
+        }
+
+        private void VerificaDocumentos(Usuario usuario)
+        {
+            var roleNoDb = _roleManager.BuscaPorId(usuario.RoleId);
+
+            if (roleNoDb == null)
+            {
+                throw new Exception("A Role não foi encontrada.");
+            }
+        }
+    }
+}
