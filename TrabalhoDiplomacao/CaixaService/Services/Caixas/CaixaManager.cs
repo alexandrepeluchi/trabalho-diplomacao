de882e4d78d9f864ce989495fea86152d4af0c58
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-11-22 15:44:11 -0300

    Manager com os metodos CRUD

---
+++ TrabalhoDiplomacao/CaixaService/Services/Caixas/CaixaManager.cs
@@ -0,0 +1,32 @@
+﻿using CaixaService.Database;
+using CaixaService.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaixaService.Services.Caixas
+{
+    public class CaixaManager : ICaixaManager
+    {
+        ApplicationDbContext _dbContext;
+
+        public CaixaManager()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        public IEnumerable<Caixa> BuscaTodos()
+        {
+            return _dbContext.Set<Caixa>().AsNoTracking();
+        }
+
+        public Caixa BuscaPorId(int id)
+        {
+            var caixa = _dbContext.Caixas.Where(x => x.Id == id)
+                                         .FirstOrDefault();
+            return caixa;
+        }
+    }
+}
